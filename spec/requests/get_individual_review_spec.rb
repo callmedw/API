1f7df91c4bf31e012be173ca18f17ad0179b9566
Dana Weiss and Nicole Santamaria <dana@>
2017-07-17 13:21:01 -0700

    add post exception spec. pass spec.

--- spec/requests/get_individual_review_spec.rb
+++ spec/requests/get_individual_review_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "get an individual review route", :type => :request do
   review = FactoryGirl.create(:review)
 
-    before { get review_path(review) }
+  before { get review_path(review) }
 
   it 'returns a confirmed status' do
     expect(response).to have_http_status(200)
