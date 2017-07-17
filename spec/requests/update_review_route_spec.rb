2ab369a6e485c213e9653d3329290cd5dc65d961
Dana Weiss and Nicole Santamaria <nicole@>
2017-07-17 16:32:37 -0700

    update spec for update route. spec will not pass.

--- spec/requests/update_review_route_spec.rb
+++ spec/requests/update_review_route_spec.rb
@@ -4,9 +4,15 @@ describe "get an individual review route", :type => :request do
   review = FactoryGirl.create(:review)
 
   before { get review_path(review) }
-  before { put review_path(review, params: { :author => 'test nicky'}) }
 
-  it 'returns a confirmed status' do
-    expect(response).to have_http_status(200)
+  after {  get :put, params: { :author => 'test nicky'} }
+
+
+  # it 'returns a confirmed status' do
+  #   expect(response).to have_http_status(200)
+  # end
+
+  it 'returns all new author' do
+    expect(JSON.parse(response.body)["author"]).to eq('test nicky')
   end
 end
