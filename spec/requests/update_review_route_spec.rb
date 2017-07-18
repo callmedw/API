20e7ced503bfd5a96dcb364d364b939e0be25ce9
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-18 08:06:27 -0700

    pass put route spec

--- spec/requests/update_review_route_spec.rb
+++ spec/requests/update_review_route_spec.rb
@@ -3,16 +3,22 @@ require 'rails_helper'
 describe "get an individual review route", :type => :request do
   review = FactoryGirl.create(:review)
 
-  before { get review_path(review) }
-
-  after {  get :put, params: { :author => 'test nicky'} }
+  # before { get review_path(review) }
+  #
+  # after {  get :put, params: { :author => 'test nicky'} }
 
 
   # it 'returns a confirmed status' do
   #   expect(response).to have_http_status(200)
   # end
 
-  it 'returns all new author' do
-    expect(JSON.parse(response.body)["author"]).to eq('test nicky')
+  # it 'returns all new author' do
+  #   expect(JSON.parse(response.body)["author"]).to eq('test nicky')
+  # end
+  it 'tests the put route' do
+    put "/reviews/#{review.id}", params: { :author => 'nicky'}
+    get "/reviews/#{review.id}"
+    # expect(Review.find(review.id).author).to eq ('nicky')
+    expect(JSON.parse(response.body)["author"]).to eq('nicky')
   end
 end
